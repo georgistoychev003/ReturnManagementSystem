c102f97a196b5dcc223bd619e1ffffc66459768a
BriceAstrion <d.orionnyamgam@gmail.com>
2023-12-09 22:30:50 +0100

    Setting up users data in js file

---
+++ template/server/data/users.js
@@ -0,0 +1,26 @@
+let users = [
+    {
+        id: 1,
+        username: '',
+        email: '',
+        password: '',
+        isAdmin: '',
+        isCollector: '',
+        isController: '',
+        isCustomer: '',
+        jwt_secret: ''
+    },
+    {
+        id: 2,
+        username: '',
+        email: '',
+        password: '',
+        isAdmin: '',
+        isCollector: '',
+        isController: '',
+        isCustomer: '',
+        jwt_secret: ''
+    }
+]
+
+export default users;
