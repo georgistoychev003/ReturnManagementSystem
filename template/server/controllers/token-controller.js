11705396c4296b7daacc1804e503362757b4d32b
Frank <540556@student.saxion.nl>
2023-12-10 13:16:22 +0100

    Login jwt function

---
+++ template/server/controllers/token-controller.js
@@ -0,0 +1,29 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import {StatusCodes} from "http-status-codes";
+
+
+const saltRounds = 10;
+
+const jwtSecret = 'ShhhhhItsASecret';
+
+export async function checkUserCredentials(req, res) {
+    const { email, password } = req.body;
+
+    const user = await getUserByEmail(email);
+
+    if (user && await bcrypt.compare(password, user.password)) {
+        const payload = {
+            email: user.email,
+            role: user.role
+        };
+
+        const token = jwt.sign(payload, jwtSecret, {
+            expiresIn: '1h'
+        });
+
+        res.status(StatusCodes.OK).json({ message: "Login successful!", token });
+    } else {
+        res.status(StatusCodes.UNAUTHORIZED).json({ error: "Invalid email or password." });
+    }
+}
