f36b9e848dc63b19a6cb6b73248bebe6b81f1229
BriceAstrion <d.orionnyamgam@gmail.com>
2023-12-12 13:37:09 +0100

    Setting user router

--- template/server/routers/user-router.js
+++ template/server/routers/user-router.js
@@ -4,7 +4,15 @@ const router = express.Router();
 
 import * as userController from '../controllers/user-controller.js';
 
+router.get('/', userController.getListOfUsers);
 
+router.get('/:emailOrUserId', userController.getUser);
+
+router.post('/', userController.postUser);
+
+router.patch('/:emailOrUserId', userController.updateUserInformation);
+
+router.delete('/:emailOrUserId', userController.deleteUser);
 
 
 export default router;
