07c88786c537429a0405f7a4568d4871f2d94937
BriceAstrion <d.orionnyamgam@gmail.com>
2023-12-21 12:43:15 +0100

    Implementation of the RMA router.

--- template/server/routers/rma-router.js
+++ template/server/routers/rma-router.js
@@ -4,13 +4,19 @@ const router = express.Router();
 
 import * as rmaController from '../controllers/rma-controller.js';
 
-// router.get('/', rmaController.getListOfRmas);
-//
-// router.get('/:rmaId', rmaController.getRma);
-//
-// router.patch('/:rmaId', rmaController.patchRma);
-//
-// router.delete('/:rmaId', rmaController.deleteRma);
+router.get('/', rmaController.getListOfRmas);
+
+router.get('/:rmaId', rmaController.getRma);
+
+router.get("/:mraId/products", rmaController.getAllReturnedProductsByRMAId);
+
+router.get("/rma/products", rmaController.getAllReturnedProducts);
+
+router.patch('/:rmaId', rmaController.patchRma);
+
+router.delete('/:rmaId', rmaController.deleteRma);
+
+router.delete("/rma/returnId/:id", rmaController.deleteRmaByReturnId);
 
 
 export default router;
